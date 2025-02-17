d6cf2e5536bd6e63fef86ec68628f237a2f87a29
Brian Ward <brianmward99@gmail.com>
2025-02-17 13:04:05 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -184,7 +184,7 @@ def deep_update(source, overrides):
     Modify ``source`` in place.
     """
     for key, value in list(overrides.items()):
-        if isinstance(value, collections.Mapping) and value:
+        if isinstance(value, collections.abc.Mapping) and value:
             returned = deep_update(source.get(key, {}), value)
             source[key] = returned
         else:
